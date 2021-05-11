570867550978b870b6f2c13c2bb790863c31e0a3
Desmond Rogers <desmondrogers2@gmail.com>
2021-05-11 15:08:53 -0500

    Add service annotation to AutosService

--- src/main/java/com/galvanize/autos/AutosService.java
+++ src/main/java/com/galvanize/autos/AutosService.java
@@ -1,7 +1,10 @@
 package com.galvanize.autos;
 
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service
 public class AutosService {
 
     AutosList autosList = new AutosList();
